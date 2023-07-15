0712a3c2bcfcf6dc35b204fe635b24756b12de92
trikto <gajanrajah@outlook.com>
2023-07-15 13:48:14 +0530

    Updating scala 03

--- Tutorial 04/21001512_03.scala
+++ Tutorial 04/21001512_03.scala
@@ -7,14 +7,23 @@ object MyClass {
         return inp.toLowerCase()
     }
 
-    def formatNames(name: String, format: String => String): String = {
-        format(name)
+    def formatNames(name: String, start: Int, end: Int, format: String => String): Unit = {
+        if (start == 0 && end == 0) {
+            println(format(name))
+        } else {
+            var startingName = name.substring(0,start)
+            var formattedName = format(name.substring(start, end))
+            var remainingName = name.substring(end)
+            var result = startingName + formattedName + remainingName
+            println(result)
+        }
+        
     }
 
     def main(args: Array[String]) = {
-        println(formatNames("Benny", toUpper))
-        println(formatNames("Niroshan", toLower))
-        println(formatNames("Saman", toUpper))
-        println(formatNames("Kumara", toLower))
+        formatNames("Benny", 0, 0, toUpper)
+        formatNames("Niroshan", 0, 2, toUpper)
+        formatNames("Saman", 0, 0, toLower)
+        formatNames("Kumara", 5, 6, toUpper)
     }
 }
